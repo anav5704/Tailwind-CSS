1e1c9b8a060aeb267f0596c865840e6751390e22
Anav Chand <anavkki@gmail.com>
2023-05-10 21:03:22 +1200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./public/*.{html,js}"],
+    theme: {
+    extend: {
+        colors: {
+            anavBlue: '#38b6ff',
+            anavGray: '#282a30',
+            anavBlack: '#151515',
+            anavWhite: '#ffffff4d',
+        },
+        fontFamily: {
+            body: ['Poppins']
+        },
+        height: {
+			"10v": "10vh",
+			"20v": "20vh",
+			"30v": "30vh",
+			"40v": "40vh",
+			"50v": "50vh",
+			"60v": "60vh",
+			"70v": "70vh",
+			"80v": "80vh",
+			"90v": "90vh",
+			"100v": "100vh",
+		},
+    },
+  },
+  plugins: [],
+}
+
