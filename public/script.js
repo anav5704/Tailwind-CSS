a316862043a124bd95c2c05e54a8eab1d2c9f8dd
Anav Chand <anavkki@gmail.com>
2023-05-22 12:06:58 +1200

    added dark mode w sessionStorage

--- public/script.js
+++ public/script.js
@@ -2,10 +2,30 @@ document.getElementById("btn").addEventListener("click", () => {
     document.getElementById("nav").classList.toggle("hidden")
 })
 
+if (sessionStorage.getItem("dark")){
+    document.documentElement.classList.add("dark");
+}
+
+if (sessionStorage.getItem("light")){
+    document.documentElement.classList.remove("dark");
+}
 
 document.getElementById("mode").addEventListener("click", () => {
-    console.log("toggle")
-    document.getElementById("light").classList.toggle("hidden");
     document.getElementById("dark").classList.toggle("hidden");
-    document.documentElement.classList.toggle("dark");
+    document.getElementById("light").classList.toggle("hidden");
+    
+    if (sessionStorage.getItem("dark")){
+        document.documentElement.classList.toggle("dark");
+        sessionStorage.setItem("light", true);
+        sessionStorage.removeItem("dark");
+        console.log("Light mode");
+    }
+
+    else {
+        document.documentElement.classList.toggle("dark");
+        sessionStorage.setItem("dark", "true");
+        sessionStorage.removeItem("light");
+        console.log("Dark mode");
+
+    }
 })
